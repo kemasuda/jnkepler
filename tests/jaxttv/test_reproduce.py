6cac1c2cada81708e2ccd110ec3266c2ab39efe5
Kento Masuda <k_masuda@dhcp184.ess.sci.osaka-u.ac.jp>
2022-08-23 09:28:49 +0900

    add tests

--- tests/jaxttv/test_reproduce.py
+++ tests/jaxttv/test_reproduce.py
@@ -13,7 +13,6 @@ path = pkg_resources.resource_filename('jnkepler', 'data/')
 def test_reproduce():
     pltag = 'kep51'
     p_init = [45.155305, 85.31646, 130.17809]
-    #name = "../../examples/kep51/ttv.txt"
     d = pd.read_csv(path+"%s_ttv.txt"%pltag, delim_whitespace=True, header=None, names=['tnum', 'tc', 'tcerr', 'dnum', 'planum'])
     tcobs = [np.array(d.tc[d.planum==j+1]) for j in range(3)]
     params_jttv = np.loadtxt(glob.glob(path+"%s*params.txt"%pltag)[0])
@@ -24,6 +23,9 @@ def test_reproduce():
     jttv.set_tcobs(tcobs, p_init)
     tc, de = jttv.get_ttvs(*params_to_elements(params_jttv, jttv.nplanet))
     assert np.max(np.abs(tc - tc_jttv)) < 1e-10
+
+    de_true = float(pd.read_csv(glob.glob(path+"%s*de.csv"%pltag)[0]).de)
+    assert de == pytest.approx(de_true)
 
 #%%
 if __name__ == '__main__':
