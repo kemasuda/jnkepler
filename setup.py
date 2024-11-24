f1e5204e8333988bd6f773c4ffa05be30aa9e60b
Kento Masuda <k_masuda@km-imac.local>
2024-11-24 18:02:41 +0900

    make ttvfast optional in tests

--- setup.py
+++ setup.py
@@ -11,8 +11,7 @@ META_PATH = os.path.join("src", "jnkepler", "__init__.py")
 CLASSIFIERS = [
     "Programming Language :: Python",
 ]
-INSTALL_REQUIRES = ["jax", "jaxoplanet", "pandas",
-                    "jaxopt>=0.8.3", "importlib_resources"]
+INSTALL_REQUIRES = ["jax", "pandas", "importlib_resources"]
 
 
 # END PROJECT SPECIFIC
