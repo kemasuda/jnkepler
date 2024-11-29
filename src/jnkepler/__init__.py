d386229c9999c7e6dfe3d6237312a52026d8afe5
Kento Masuda <k_masuda@km-imac.local>
2024-11-29 19:46:02 +0900

    v0.2

--- src/jnkepler/__init__.py
+++ src/jnkepler/__init__.py
@@ -1,11 +1,11 @@
 __all__ = []
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __uri__ = "https://github.com/kemasuda/jnkepler"
 __author__ = "Kento Masuda"
 __email__ = "kmasuda@ess.sci.osaka-u.ac.jp"
 __license__ = "MIT"
-__description__ = "modeling non-keplerian dynamics with jax"
+__description__ = "JAX code for modeling nearly-Keplerian orbits"
 
 from . import jaxttv
 from . import nbodytransit
