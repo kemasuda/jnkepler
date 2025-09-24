4108983d9e6d702351ea48baa0e8073f64feb8ab
Kento Masuda <k_masuda@dhcp233.ess.sci.osaka-u.ac.jp>
2025-09-24 20:06:52 +0900

    warning on thunk runtime

--- src/jnkepler/__init__.py
+++ src/jnkepler/__init__.py
@@ -1,6 +1,5 @@
 __all__ = []
 
-# __version__ = "0.2.0"
 __uri__ = "https://github.com/kemasuda/jnkepler"
 __author__ = "Kento Masuda"
 __email__ = "kmasuda@ess.sci.osaka-u.ac.jp"
@@ -12,3 +11,23 @@ from . import jaxttv
 from . import nbodytransit
 from . import nbodyrv
 from . import infer
+
+import os
+import warnings
+
+if "--xla_cpu_use_thunk_runtime=false" not in os.environ.get("XLA_FLAGS", ""):
+    warnings.warn(
+        'For best CPU performance on JAX >= 0.4.32, please disable the thunk runtime.\n'
+        '\n'
+        'You can do this by setting the environment variable BEFORE importing jax:\n'
+        '    export XLA_FLAGS="--xla_cpu_use_thunk_runtime=false"\n'
+        '\n'
+        'Or inside Python, add these lines before importing jax:\n'
+        '    import os\n'
+        '    os.environ["XLA_FLAGS"] = "--xla_cpu_use_thunk_runtime=false"\n'
+        '    import jax\n'
+        '\n'
+        'Without this, CPU execution may be significantly slower, especially when computing gradients.',
+        UserWarning,
+    )
+
