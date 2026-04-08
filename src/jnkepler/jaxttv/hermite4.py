fix NaN gradients in hermite4 get_derivs and get_energy using safe double-where

src/jnkepler/jaxttv/hermite4.py
@@ -32,11 +32,12 @@ def get_derivs(x, v, masses):
     x2jk = jnp.sum(xjk * xjk, axis=1)[:, None, :]
     xvjk = jnp.sum(xjk * vjk, axis=1)[:, None, :]
 
-    x2jk = jnp.where(x2jk != 0., x2jk, jnp.inf)
-    x2jkinv = 1. / x2jk
+    nz = x2jk != 0.
+    x2jk_safe = jnp.where(nz, x2jk, 1.0)
+    x2jkinv = 1. / x2jk_safe
     x2jkinv1p5 = x2jkinv * jnp.sqrt(x2jkinv)
-    Xjk = - xjk * x2jkinv1p5
-    dXjk = (- vjk + 3 * xvjk * xjk * x2jkinv) * x2jkinv1p5
+    Xjk = jnp.where(nz, - xjk * x2jkinv1p5, 0.0)
+    dXjk = jnp.where(nz, (- vjk + 3 * xvjk * xjk * x2jkinv) * x2jkinv1p5, 0.0)
 
     a = G * jnp.dot(Xjk, masses)
     adot = G * jnp.dot(dXjk, masses)

src/jnkepler/jaxttv/utils.py
@@ -183,8 +183,12 @@ def get_energy(x, v, masses):
     """
     K = jnp.sum(0.5 * masses * jnp.sum(v*v, axis=1))
     X = x[:, None] - x[None, :]
+    d2 = jnp.sum(X*X, axis=2)
+    mask = jnp.tril(jnp.ones_like(d2, dtype=bool), k=-1)
+    d2_safe = jnp.where(mask, d2, 1.0)
+    inv_r = jnp.where(mask, 1.0 / jnp.sqrt(d2_safe), 0.0)
     M = masses[:, None] * masses[None, :]
-    U = -G * jnp.sum(M * jnp.tril(1./jnp.sqrt(jnp.sum(X*X, axis=2)), k=-1))
+    U = -G * jnp.sum(M * inv_r)
     return K + U
 
 